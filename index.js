15abbd7b34dde26e3fb3d6ba858d2b5e8476495f
Kristine Steine <kristine.steine@bekk.no>
2017-08-03 21:26:37 +0200

    Add simple yr endpoint

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.json({
+app.get('/', (req, res) => {
+  res.json({
     messages: [
       {text: "Dette er en svarmelding!"}
     ]
   });
 });
 
-app.listen(app.get('port'), function() {
+app.get('/yr/idag', (req, res) => {
+  res.json({
+    messages: [
+      {text: "Kan være greit å ha med paraply i dag! ☔"}
+    ]
+  });
+});
+
+app.listen(app.get('port'), () => {
   console.log('Tenkbot-API is running on port', app.get('port'));
 });
